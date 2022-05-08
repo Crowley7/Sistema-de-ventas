e0b89bc8484e99026038a15a864636102526f800
Elias Reyes <elias.rey2526@gmail.com>
2022-05-08 12:02:47 -0500

    version inicial del proyecto

---
+++ inc/footer.php
@@ -0,0 +1,47 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Equipo de alta calidad.</br>
+                    Enfocados en la atención al clientet.</br>
+                    Competencia en tecnología.</br>
+                    Innovaciones en el Soporte Técnico.</br>
+                    Hardware de última generación.</br>
+                    Garantía de satisfacción.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Perú</p>
+                <p style="color: #FFF">Lima - Chorrillos / Villa el Salvador</p>
+                <p style="font-size:20px" aria-hidden="true"> 989311276 / 984913206</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  987654321 </p> <a href="#" target="_blank" style="color: #5bc0de"> envianos_un_mensaje </a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; freedom@gaming.pe </a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">FREEDOM &copy; 2021</h5>
+</footer>
